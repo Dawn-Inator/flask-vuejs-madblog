776e2a2436996b4df7f5b70266b0bebbdbe92489
Dawn Inator <126039858+Dawn-Inator@users.noreply.github.com>
2024-03-02 21:49:11 +0800

    Update __init__.py

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.cdconfig.from_object(config_class)
+    app.config.from_object(config_class)
 
     # 注册 blueprint
     from app.api import bp as api_bp
