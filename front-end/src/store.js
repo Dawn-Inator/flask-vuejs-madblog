0bf791931cba95431d45f74909ad7a122310d0a6
Dawn Inator <dawn.inator.lzl@gmail.com>
2024-03-10 20:11:34 +0800

    6. 博客文章CURD与Markdown

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,37 +1,19 @@
-function parseToken(token) {
-  try {
-    if (!token) {
-      return { user_id: 0 }; // 如果没有token，直接返回默认user_id
-    }
-    const base64Url = token.split('.')[1];
-    if (!base64Url) {
-      throw new Error('Invalid token'); // 如果token格式不正确，抛出错误
-    }
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const payload = JSON.parse(atob(base64));
-    return payload;
-  } catch (e) {
-    console.error('Failed to parse the token:', e);
-    return { user_id: 0 }; // 解析失败时，返回默认user_id
-  }
-}
-
 export default {
   debug: true,
   state: {
-    is_authenticated: !!window.localStorage.getItem('madblog-token'),
-    user_id: parseToken(window.localStorage.getItem('madblog-token')).user_id,
+    is_authenticated: window.localStorage.getItem('madblog-token') ? true : false,
+    // 用户登录后，就算刷新页面也能再次计算出 user_id
+    user_id: window.localStorage.getItem('madblog-token') ? JSON.parse(atob(window.localStorage.getItem('madblog-token').split('.')[1])).user_id : 0
   },
-  loginAction() {
-    if (this.debug) console.log('loginAction triggered');
-    const payload = parseToken(window.localStorage.getItem('madblog-token'));
-    this.state.is_authenticated = true;
-    this.state.user_id = payload.user_id;
+  loginAction () {
+    if (this.debug) { console.log('loginAction triggered') }
+    this.state.is_authenticated = true
+    this.state.user_id = JSON.parse(atob(window.localStorage.getItem('madblog-token').split('.')[1])).user_id
   },
-  logoutAction() {
-    if (this.debug) console.log('logoutAction triggered');
-    window.localStorage.removeItem('madblog-token');
-    this.state.is_authenticated = false;
-    this.state.user_id = 0;
+  logoutAction () {
+    if (this.debug) console.log('logoutAction triggered')
+    window.localStorage.removeItem('madblog-token')
+    this.state.is_authenticated = false
+    this.state.user_id = 0
   }
 }
